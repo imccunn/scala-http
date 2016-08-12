f0a404e3d96921fbbf36ce30f7166cad17b7fb5c
Ian D. McCunn <imccunn@expedia.com>
2016-08-12 10:30:57 -0700

    test

--- src/main/java/Hello.scala
+++ src/main/java/Hello.scala
@@ -1,14 +1,12 @@
 
 import org.apache.http.client.HttpClient
-import org.apache.http.{HttpEntity, HttpRequest}
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
-import org.apache.http.impl.client.{CloseableHttpClient, DefaultHttpClient, HttpClientBuilder, HttpClients}
+import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
-import org.json4s._
 import org.json4s.jackson.Serialization
 import org.json4s.jackson.Serialization._
-import org.json4s.{DefaultFormats, NoTypeHints}
+import org.json4s.NoTypeHints
 
 import scala.util.Try
 
@@ -20,26 +18,25 @@ object Hello extends App {
   implicit val formats = Serialization.formats(NoTypeHints)
 
   val url = "http://localhost:3333/data"
-  val payload = Map[String, String](
-    "data" -> "neat",
-    "cool"-> "wow",
-    "answer" -> "10"
+  val payload = Map[String, Either[String, Int]](
+    "id" -> Right(2232),
+    "cool"-> Left("beans"),
+    "data" -> Right(1234322)
   )
   println(payload)
   val json = write(payload)
   println(json)
 
-  val responseString = httpPost(json)
+  val responseString = httpPost(json, url)
 
   println(responseString)
-  println("End")
 
-  def httpPost(payload:String):Try[String] =
+  def httpPost(payload:String, url:String):Try[String] =
     Try {
       implicit val formats = Serialization.formats(NoTypeHints)
       println(s"Attempting http POST with payload: $payload")
       val httpclient:HttpClient = HttpClients.createDefault()
-      val post:HttpPost = new HttpPost("http://localhost:3333/data")
+      val post:HttpPost = new HttpPost(url)
       val entity:StringEntity = new StringEntity(payload)
       post.setEntity(entity)
       val res = httpclient.execute(post)
